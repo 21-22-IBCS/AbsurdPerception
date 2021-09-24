6a2e4555c61f60892ec57a96d2aaaf9cb29bb121
Sofiia Syzonenko <sofiia_syzonenko@m018656.aw.org>
2021-09-24 14:39:53 -0700

    python

---
+++ pythonBasics.py
@@ -0,0 +1,40 @@
+def main():
+
+    #Part 1
+
+    print("Python Basic Commands Practice")
+    print("9/22/2021")
+
+    #Part 2
+
+    x = 20
+    y = 35
+
+    print(x+y)
+
+    #Part 3
+
+    x = 45
+    y = 65
+
+    print(x*y)
+
+    #Part 4
+    #Cannot concatenate int types to str types
+
+    var = 157
+    print(" There are curently number " + str(var) + " of students in the USG ")
+
+    girl = 20
+    print(" There are curently number " + str(girl) + " of students in the USG ")
+
+    people = 80
+    print(" There are curently number " + str(people) + " of students in the USG ")
+    
+    
+    
+    
+
+if __name__ == "__main__":
+        main()
+    
