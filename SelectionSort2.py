5218d292f3b3fc37230656f148c4d68875aa9bcb
sofiiasyzonenko <89945149+sofiiasyzonenko@users.noreply.github.com>
2021-10-21 21:15:56 -0700

    Add files via upload

---
+++ SelectionSort2.py
@@ -0,0 +1,14 @@
+def selection_sort(aList):
+    for i in range(len(aList)):
+        min_index = i
+        for j in range(i+1, len(aList)):
+            if aList[j] < aList[min_index]:
+                min_index = j
+
+        aList[i], aList[min_index] = aList[min_index], aList[i]
+
+aList = [3,5,45,67,90,34,56,789,39,68,25]
+print(aList)
+
+selection_sort(aList)
+print(aList)
