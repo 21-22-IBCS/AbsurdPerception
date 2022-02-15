54176c77b26dfac25cba1f2c784f77057458fa57
sofiiasyzonenko <89945149+sofiiasyzonenko@users.noreply.github.com>
2022-02-15 14:34:25 -0800

    Add files via upload

--- conversion lab.py
+++ conversion lab.py
@@ -1,11 +1,4 @@
 def main() :
-
-    response = input("Type an amount of usd dollars")
-    response = int(response)
-    #for response in range(0,5):
-        #if response == exit
-            #print(exit)
-            #exit()
     d = {
         "ukrainian hryvnia" : 27.98,
         "russian ruble" : 75.32,
@@ -14,10 +7,18 @@ def main() :
         "pound sterling" : 0.74,
         "poland złoty" : 3.96
         }
-    print("ukrainian hryvnia","russian ruble", "euro", "chinese yuan", "pound sterling", "poland złoty")
-    choice = input("choose one of the options")
-    conversion = d.get(choice)*response
-    print(conversion)
+
+    while True:
+        
+        response = input("Type an amount of usd dollars")
+        response = int(response)
+        print("ukrainian hryvnia","russian ruble", "euro", "chinese yuan", "pound sterling", "poland złoty")
+        choice = input("choose one of the options")
+        conversion = d.get(choice)*response
+        print(conversion)
+        question = input("do you want to continue?")
+        if question == "no":
+            break
 
 
 if __name__ == "__main__":
